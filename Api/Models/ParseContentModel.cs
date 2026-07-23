a764af0e8c1bb73bca6f2dfabc6c41dd830a7cb9
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-07-23 16:47:17 +0200

    fix: `ContentType.Csv` being default value for `type`

--- Api/Models/ParseContentModel.cs
+++ Api/Models/ParseContentModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Models;
 
 public class ParseContentModel
 {
-    public ContentType Type { get; set; }
+    [Required(ErrorMessage = "Field 'type' is required")]
+    public ContentType? Type { get; set; }
+
+    [Required(ErrorMessage = "Field 'content' is required")]
     public string Content { get; set; } = string.Empty;
 }
