55fc709d9a49955f27d9b4d933ab221248e87e6c
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-07-23 17:18:27 +0200

    test: add integration tests for `/api/v1/parse-content` route

---
+++ Api.Tests/ParseContentTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Api.Tests;
+
+public class ParseContentTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Theory]
+    [InlineData("CSV", "123")]
+    [InlineData("INTERNAL_JSON", "456")]
+    public async Task PostReturnsOk(string type, string content)
+    {
+        var body = new { type, content };
+        var response = await _client.PostAsJsonAsync("/api/v1/parse-content", body);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(null, "123")]
+    [InlineData("CSV", null)]
+    [InlineData("something", "else")]
+    [InlineData("csv", "...")]
+    [InlineData("iNtErNaL_jSoN", "hello_world")]
+    public async Task PostReturnsBadRequest(string? type, string? content)
+    {
+        var body = new { type, content };
+        var response = await _client.PostAsJsonAsync("/api/v1/parse-content", body);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
