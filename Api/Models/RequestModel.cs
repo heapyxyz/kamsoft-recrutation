caf6ca48e59e84834e384e41d1747e5a845ce9e0
heapyxyz <63428380+heapyxyz@users.noreply.github.com>
2026-07-27 22:41:02 +0200

    refactor: make `content` nullable (but still required)

--- Api/Models/RequestModel.cs
+++ Api/Models/RequestModel.cs
@@ -8,5 +8,5 @@ public class RequestModel
     public ContentType? Type { get; set; }
 
     [Required(ErrorMessage = "Field 'content' is required")]
-    public string Content { get; set; } = string.Empty;
+    public string? Content { get; set; }
 }
