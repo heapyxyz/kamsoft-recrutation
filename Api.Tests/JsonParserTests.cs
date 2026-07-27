fc69f8cfa31c2a27425b144cbed65ed4429656ae
heapyxyz <63428380+heapyxyz@users.noreply.github.com>
2026-07-27 12:01:31 +0200

    feat: json parser service tests

---
+++ Api.Tests/JsonParserTests.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using Api.Services;
+
+namespace Api.Tests;
+
+public class JsonParserTests
+{
+    private readonly JsonParserService _service = new();
+
+    [Theory]
+    [InlineData(2, "{ \"hello\": \"world\", \"this_is\": \"a_test\" }")]
+    public void ParseObjectReturnsCorrectCount(int expectedNumber, string content)
+    {
+        ParseResult result = _service.Parse(content);
+        Assert.Equal(expectedNumber, result.ParsedNumber);
+    }
+
+    [Theory]
+    [InlineData(5, "[1, 2, 3, 4, 5]")]
+    public void ParseArrayReturnsCorrectCount(int expectedNumber, string content)
+    {
+        ParseResult result = _service.Parse(content);
+        Assert.Equal(expectedNumber, result.ParsedNumber);
+    }
+
+    [Theory]
+    [InlineData("123")]
+    [InlineData("10")]
+    [InlineData("1")]
+    public void ParsePrimitiveReturnsOne(string content)
+    {
+        ParseResult result = _service.Parse(content);
+        Assert.Equal(1, result.ParsedNumber);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("not json")]
+    [InlineData("{ hello: world }")]
+    [InlineData("{ \"hello\": \"world }")]
+    [InlineData("{ \"hello\": \"world\"")]
+    public void ParseInvalidJsonThrowsException(string content)
+    {
+        Assert.ThrowsAny<JsonException>(() => _service.Parse(content));
+    }
+}
